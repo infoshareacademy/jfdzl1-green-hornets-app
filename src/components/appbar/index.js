c72dd5d717e3f3a2df1075320d3d79b89a998cdb
darqoo <d.smolag@gmail.com>
2017-11-26 13:02:35 +0100

    starting app

---
+++ src/components/appbar/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class AppBar extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default AppBar;
---
+++ src/components/sidebar/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Sidebar extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default Sidebar;
