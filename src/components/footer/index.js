4c5ecd791ee78da7a1fd8905d2f36c6d269bffe5
darqoo <d.smolag@gmail.com>
2017-12-03 13:32:47 +0100

    small changes

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,21 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './index.css';
 
+const today = new Date();
+
 class Footer extends Component {
+
     render() {
         return (
             <div className="App-footer">
-                <p className="App-footer-rights">&copy;All rights reserved-Lubeer</p>
+                <p className="App-footer-rights">&copy; {today.getFullYear()} All rights reserved - Lubeer</p>
                 <ul className="App-footer-containerIcons">
-
-                    <li className="App-footer-socialIcon"><a
-                        href="https://github.com/infoshareacademy/jfdzl1-green-hornets-www" target="_blank"><i
-                        class="fa fa-github"></i></a></li>
-                    <li className="App-footer-socialIcon"><a href="https://www.facebook.com/" target="_blank"><i
-                        class="fa fa-facebook-square"></i></a></li>
+                    <li className="App-footer-socialIcon">
+                        <a href="https://github.com/infoshareacademy/jfdzl1-green-hornets-app" target="_blank">
+                        <i className="fa fa-github"></i>
+                        </a>
+                    </li>
+                    <li className="App-footer-socialIcon">
+                        <a href="https://www.facebook.com/" target="_blank">
+                        <i className="fa fa-facebook-square"></i>
+                        </a>
+                    </li>
                 </ul>
             </div>
         );
     }
 }
- export default Footer;
+
+export default Footer;
