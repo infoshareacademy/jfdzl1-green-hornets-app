a8b3776e9c171e4adb1074e50a5af852a486013e
darqoo <d.smolag@gmail.com>
2017-12-03 13:57:14 +0100

    small changes

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './index.css';
 
 const today = new Date();
@@ -9,18 +9,12 @@ class Footer extends Component {
         return (
             <div className="App-footer">
                 <p className="App-footer-rights">&copy; {today.getFullYear()} All rights reserved - Lubeer</p>
-                <ul className="App-footer-containerIcons">
-                    <li className="App-footer-socialIcon">
-                        <a href="https://github.com/infoshareacademy/jfdzl1-green-hornets-app" target="_blank">
-                        <i className="fa fa-github"></i>
-                        </a>
-                    </li>
-                    <li className="App-footer-socialIcon">
-                        <a href="https://www.facebook.com/" target="_blank">
-                        <i className="fa fa-facebook-square"></i>
-                        </a>
-                    </li>
-                </ul>
+                <a href="https://github.com/infoshareacademy/jfdzl1-green-hornets-app" target="_blank">
+                    <i className="fa1 fa fa-github"></i>
+                </a>
+                <a href="https://www.facebook.com/" target="_blank">
+                    <i className="fa2 fa fa-facebook-square"></i>
+                </a>
             </div>
         );
     }
