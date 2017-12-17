084e0ad792df63b036758cb83f4c2292ab7674ce
Patryk Omiotek <patryk.omiotek@gmail.com>
2017-12-17 10:46:08 +0100

    added material ui

--- src/components/appbar/index.js
+++ src/components/appbar/index.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Grid from 'material-ui/Grid';
 import './index.css';
 
 class AppBar extends Component {
-    render() {
-        return (
-            <div></div>
-        );
-    }
+  render() {
+    return (
+      <Grid item xs={12}>
+
+      </Grid>
+    );
+  }
 }
 
 export default AppBar;
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Grid from 'material-ui/Grid';
+
 import './index.css';
 
 class Dashboard extends Component {
-    render() {
-        return (
-            <div></div>
-        );
-    }
+  render() {
+    return (
+      <Grid item xs={12}>
+
+      </Grid>
+    );
+  }
 }
 
 export default Dashboard;
