49691aeb905f567d4fd29b295efe6bc27b94cd00
Patryk Omiotek <patryk.omiotek@gmail.com>
2017-12-17 11:02:27 +0100

    added material ui paper

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Grid from 'material-ui/Grid';
+import Paper from 'material-ui/Paper';
 
 import './index.css';
 
@@ -7,7 +8,9 @@ class Dashboard extends Component {
   render() {
     return (
       <Grid item xs={12}>
-
+        <Paper>
+          <h1>Dashboard</h1>
+        </Paper>
       </Grid>
     );
   }
