56c8aabfdf1ca4096e1b6045694c5b684f243f82
Krzysztof Durak <krzysztofdurak@gmail.com>
2017-12-17 13:44:40 +0100

    added appbar and palete color

--- src/components/appbar/index.js
+++ src/components/appbar/index.js
@@ -1,15 +1,51 @@
 import React, {Component} from 'react';
 import Grid from 'material-ui/Grid';
 import './index.css';
+import PropTypes from 'prop-types';
+import {withStyles} from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import primaryGreen from '../palete'
 
-class AppBar extends Component {
-  render() {
+const styles = {
+    root: {
+        width: '100%',
+    },
+    flex: {
+        flex: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+
+};
+
+function ButtonAppBar(props) {
+    const {classes} = props;
     return (
-      <Grid item xs={12}>
-
-      </Grid>
+        <div className={classes.root}>
+            <AppBar position="static" style={primaryGreen} >
+                <Toolbar>
+                    <IconButton className={classes.menuButton} color="contrast" aria-label="Menu">
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography type="title" color="inherit" className={classes.flex}>
+                        LUBEER
+                    </Typography>
+                    <Button color="contrast">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
     );
-  }
 }
 
-export default AppBar;
+ButtonAppBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonAppBar);
