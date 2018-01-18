350a210a8afe535f1abb016a61c2936941187b68
Patryk Omiotek <patryk.omiotek@gmail.com>
2018-01-18 13:47:04 +0100

    appar and sidebar

--- src/components/appbar/index.js
+++ src/components/appbar/index.js
@@ -1,61 +1,46 @@
-import React, {Component} from 'react';
-import Grid from 'material-ui/Grid';
-import './index.css';
-import PropTypes from 'prop-types';
-import {withStyles} from 'material-ui/styles';
+import React from 'react';
+import {connect} from 'react-redux';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
-import primaryGreen from '../palete'
-import {showNotification, toggleSidebar} from '../../UI/logic';
-import {connect} from 'react-redux';
 
+import primaryGreen from '../palete'
+import {toggleSidebar} from '../../UI/logic';
 
 const styles = {
-    root: {
-        width: '100%'
-    },
-    flex: {
-        flex: 1
-    },
-    menuButton: {
-        marginLeft: -12,
-        marginRight: 20
-    }
+  root: {
+    width: '100%'
+  },
+  flex: {
+    flex: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        showSnackbar: (message) => dispatch(showNotification('Hello iSA :)')),
-        toggleSidebar: () => dispatch(toggleSidebar())
-    }
-}
+const mapDispatchToProps = dispatch => ({
+  toggleSidebar: () => dispatch(toggleSidebar())
+});
 
-class ButtonAppBar extends Component {
-    render() {
-        return (
-            <div style={styles.root}>
-                <AppBar position="static" style={primaryGreen}>
-                    <Toolbar>
-                        <IconButton style={styles.menuButton} color="contrast" aria-label="Menu">
-                            <MenuIcon onClick={this.props.toggleSidebar}/>
-                        </IconButton>
-                        <Typography type="title" color="inherit" style={styles.flex}>
-                            LUBEER
-                        </Typography>
-                        <Button color="contrast">Login</Button>
-                    </Toolbar>
-                </AppBar>
-            </div>
-        );
-    }
-}
-
-ButtonAppBar.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
+const ButtonAppBar = (props) => (
+  <div style={styles.root}>
+    <AppBar position="static" style={primaryGreen}>
+      <Toolbar>
+        <IconButton style={styles.menuButton} color="contrast" aria-label="Menu">
+          <MenuIcon onClick={props.toggleSidebar}/>
+        </IconButton>
+        <Typography type="title" color="inherit" style={styles.flex}>
+          LUBEER
+        </Typography>
+        <Button color="contrast">Login</Button>
+      </Toolbar>
+    </AppBar>
+  </div>
+);
 
 export default connect(null, mapDispatchToProps)(ButtonAppBar);
